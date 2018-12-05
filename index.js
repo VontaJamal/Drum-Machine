89d3002e3e9716b27b0528807575f1bb06f358ab
Vonta Johnson <devontajohnsonit@gmail.com>
2018-12-05 16:11:31 -0500

    added in some loop functionality

--- index.js
+++ index.js
@@ -5,19 +5,37 @@ const keys = Array.from(document.querySelectorAll("div[data-key]"));
 const playButton = document.querySelector(".play");
 const resetButton = document.querySelector(".reset");
 const recordButton = document.querySelector(".record");
-let updateTime;
-
+const loopButton = document.querySelector(".loop");
+const recordTime = document.querySelector(".recordTime");
 let isRecording = false;
+let isLooping = false;
+let updateTime;
 
 window.addEventListener("keydown", playSound);
 playButton.addEventListener("click", playRecording);
 resetButton.addEventListener("click", clearRecording);
 recordButton.addEventListener("click", record);
+loopButton.addEventListener("click", loop);
+
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+function removeTransition(e) {
+  e.target.classList.remove("playing");
+  e.target.style.color = "#fdfdfd";
+  e.target.style.borderColor = "#fdfdfd";
+}
+
+function loop() {
+  isLooping = !isLooping;
+  loopButton.classList.add("looping");
+
+  if (!isLooping) {
+    loopButton.classList.remove("looping");
+  }
+}
 
 function record() {
   const recordText = document.querySelector(".recordText");
-  const recordTime = document.querySelector(".recordTime");
   let counter = 0;
 
   isRecording = !isRecording;
@@ -42,9 +60,16 @@ function record() {
 function playRecording() {
   //check if we have recorded sounds
   if (sounds.length > 0) {
+    isRecording = false;
     let start = 0;
+    let counter = 0;
 
-    sounds[start].play();
+    updateTime = setInterval(function count() {
+      counter++;
+      formatCount(counter);
+    }, 1000);
+
+    playSound(sounds[start]);
 
     sounds[start].addEventListener("ended", function soundPlayer() {
       playNextSound(start + 1);
@@ -75,17 +100,22 @@ function formatCount(seconds) {
   }
 }
 
-function removeTransition(e) {
-  e.target.classList.remove("playing");
-}
-
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const player = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  let audio, player;
+
+  if (e) {
+    audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) || e;
+    player =
+      document.querySelector(`div[data-key="${e.keyCode}"]`) ||
+      document.querySelector(`div[data-key="${e.dataset.key}"]`);
+  }
 
   if (!player || !audio) return;
+  let color = randomColor();
 
   player.classList.add("playing");
+  player.style.color = color;
+  player.style.borderColor = color;
   audio.currentTime = 0;
   audio.play();
 
@@ -97,13 +127,36 @@ function playSound(e) {
 function playNextSound(index) {
   //recording has finished
   if (!sounds[index]) {
-    return;
+    if (isLooping) {
+      index = 0;
+    } else {
+      clearInterval(updateTime);
+      setTimeout(function resetTime() {
+        recordTime.innerHTML = "Recording Finished!";
+
+        setTimeout(function resetTime() {
+          recordTime.innerHTML = "0:00";
+        }, 1000);
+      }, 1000);
+
+      return;
+    }
   }
 
-  sounds[index].play();
+  playSound(sounds[index]);
 
   sounds[index].addEventListener("ended", function soundPlayer() {
     playNextSound(index + 1);
     sounds[index].removeEventListener("ended", soundPlayer);
   });
+}
+
+function randomColor() {
+  let red, green, blue;
+
+  red = Math.floor(Math.random() * 256);
+  green = Math.floor(Math.random() * 256);
+  blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red},${green},${blue})`;
 }
